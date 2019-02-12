92678e8f3aa5f56edffbe4ff29fc7b19d2efe2ec
Elise Phan <aliceconnic@gmail.com>
2019-02-12 01:10:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :authors, only: [:new, :show]
-  get 'authors/:id/edit' to 'authors#edit', as edit_author
+  get 'authors/:id/edit' to 'authors#edit', as: :edit_author
   patch 'authors/:id', to 'authors#update'
 
 end
