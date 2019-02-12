3c569088430f519de715cf3b4d20550054c50ab7
Elise Phan <aliceconnic@gmail.com>
2019-02-12 01:53:25 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -23,8 +23,11 @@ class AuthorsController < ApplicationController
   
   def update
     @author = Author.find(params[:id])
-    @author.update(author_params)
+    if @author.update(author_params)
     redirect_to author_path(@author)
+    else
+      render :edit 
+    end
   end 
   
 
